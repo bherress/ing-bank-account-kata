7bc870f517156f08be319ffbee2c2a34a0ee4290
bherress <badr.herress@capgemini.com>
2020-09-06 11:40:13 +0200

    [feat] init kata project

---
+++ src/test/java/fr/ing/interview/KataApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ing.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
